592c13d55ca631e9bd00daab15677b16a464f0e5
Nihal Mohamed Bashir <82413665+voidx23@users.noreply.github.com>
2025-08-15 20:28:31 +0400

    eslint rule error
    an error caused by eslint rule cleared

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,7 @@ const Home = () => {
               Software Developer
             </span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Nihal Mohamed Bashir</span>
+              Hello I&apos;m <br /> <span className="text-accent">Nihal Mohamed Bashir</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies. </p>
             {/* btn and socials */}
